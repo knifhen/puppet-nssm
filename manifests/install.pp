16c887bb5cd3070c1b6da71778b0c548daf1b153
Andreas Knifh <aknifh@williamsinteractive.com>
2014-11-06 11:19:30 +0100

    fixed syntax error in unzip-nssm command create the $nssm::install_dir before executing unzip-nssm

--- manifests/install.pp
+++ manifests/install.pp
@@ -8,23 +8,25 @@
 # It downloads the package and installs it.
 #
 class nssm::install {
+  $download_dir = 'c:\\Windows\\Temp'
+  $download_file = "nssm-${nssm::version}.zip"
+  $download_path = regsubst("${download_dir}\\${download_file}", '\\\\', '\\', 'G')
+  $install_dir_win = regsubst("${nssm::install_dir}", '\\\\', '\\', 'G')
+
+  file { $nssm::install_dir: ensure => directory; }
 
   download_file { 'nssm-download':
     url                   => $nssm::install_url,
-    destination_directory => 'C:\\Windows\\Temp'
+    destination_directory => $download_dir,
   }
 
   case $::architecture {
-    'x86_64', 'amd64': {
-      $bitness = '64'
-    }
-    default: {
-      $bitness = '32'
-    }
+    'x86_64', 'amd64' : { $bitness = '64' }
+    default           : { $bitness = '32' }
   }
 
   exec { 'unzip-nssm':
-    command  => "\$shell = New-Object -COMObject Shell.Application; \$zipfile = \$shell.NameSpace(\"nssm-${nssm::version}.zip\"); foreach(\$item in \$zipfile.Items()){ \$shell.NameSpace(\"${nssm::install_dir}\").CopyHere(\$item, 20)",
+    command  => "\$shell = New-Object -COMObject Shell.Application; \$zipfile = \$shell.NameSpace(\"${download_path}\"); foreach(\$item in \$zipfile.Items()){ \$shell.NameSpace(\"${install_dir_win}\").CopyHere(\$item, 20)};",
     provider => powershell,
     creates  => "${nssm::install_dir}\\nssm-${nssm::version}\\win${bitness}",
     require  => Download_file['nssm-download']
